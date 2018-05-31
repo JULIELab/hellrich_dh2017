713b78135d7b2ca7db36c68561a39e825fd482a0
Johannes Hellrich <johannes.hellrich@uni-jena.de>
2018-05-31 19:18:34 +0200

    Delete eval-random.sh

--- eval-random.sh
+++
@@ -1,9 +0,0 @@
-HYPERWORD_PATH="/home/hellrich/hyperwords/omerlevy-hyperwords-688addd64ca2/hyperwords"
-
-mkdir -p log
-rm -f log/pmi_random
-for x in 1 2 3
-do
-        echo $x >> log/pmi_random
-        python $HYPERWORD_PATH/closest-hyper.py --w+c --eig 0 SVD pmi_random_v$x/svd_pmi frau gott herr herz liebe mann mutter nacht tag wein >> log/pmi_random
-done
